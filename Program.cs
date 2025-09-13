ed1fb3453cd17365b6f170fcb256558423473b98
lenovo <lenovo@DESKTOP-LT8OTFB>
2025-09-13 00:11:17 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chava;
+
+namespace BankTel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Amount");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Class1 obj = new Class1();
+            obj.GetBalance(x);
+            Console.WriteLine(" End");
+        }
+    }
+}
